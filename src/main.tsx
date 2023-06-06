94c45520caa86b9112a1e46cf1ab3883b3582c98
Panto! <guustavopantoja@gmail.com>
2023-06-06 08:10:02 -0300

    feat: implement title bar

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import TitleBar from "./components/TitleBar";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <TitleBar />
     <App />
   </React.StrictMode>
 );
