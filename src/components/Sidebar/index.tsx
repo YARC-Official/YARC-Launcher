c0c89cbd2bc19919c8ebf37e1c59b4bebad4b14d
Panto! <guustavopantoja@gmail.com>
2023-06-07 18:11:57 -0300

    feat: menu settings

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,9 +2,19 @@ import styles from './Sidebar.module.css';
 import { ReactComponent as DiscordIcon } from '@app/assets/Discord.svg';
 import { ReactComponent as TwitterIcon } from '@app/assets/Twitter.svg';
 import { ReactComponent as GithubIcon } from '@app/assets/Github.svg';
+import { ReactComponent as NewsIcon } from '@app/assets/News.svg';
+import { ReactComponent as SettingsIcon } from '@app/assets/Settings.svg';
+import SidebarMenuButton from './SidebarMenuButton';
+import { Link } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
   return <div className={styles.sidebar}>
+
+    <div className={styles.menus}>
+      <Link to="/"><SidebarMenuButton>Home</SidebarMenuButton></Link>
+      <Link to="/news"><SidebarMenuButton icon={<NewsIcon />}>News</SidebarMenuButton></Link>
+      <Link to="/settings"><SidebarMenuButton icon={<SettingsIcon />}>Settings</SidebarMenuButton></Link>      
+    </div>
 
     <div className={styles.footer}>
       <div className={styles.credits}>YARC 2023</div>
