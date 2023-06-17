26b6c876501384e93735ea5e1e376cf572bd273d
Panto! <guustavopantoja@gmail.com>
2023-06-17 13:41:43 -0300

    feat: add local (offline) storage for react query

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,8 @@ import TitleBar from "./components/TitleBar";
 import { RouterProvider } from "react-router-dom";
 import Router from "@app/routes";
 import { invoke } from "@tauri-apps/api/tauri";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-        },
-    },
-});
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./query";
 
 try {
     // Idk how react works, but this should show a loading screen or something
---
+++ src/query.ts
@@ -0,0 +1,19 @@
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            cacheTime: Infinity
+        },
+    },
+});
+
+const localStoragePersister = createSyncStoragePersister({ storage: window.localStorage });
+
+persistQueryClient({
+    queryClient,
+    persister: localStoragePersister,
+});
