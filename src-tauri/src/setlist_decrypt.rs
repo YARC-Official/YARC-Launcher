d88fdf23400cb55b97412bb383cb373602efd32c
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-06-24 14:08:17 -0400

    Setlist downloading stuff

--- src-tauri/src/setlist_decrypt.rs
+++ src-tauri/src/setlist_decrypt.rs
@@ -1,9 +1,26 @@
 use std::path::Path;
 
-const SETLIST_ZIP_KEY: &str = "this_is_a_test_password";
+use sevenz_rust::Password;
+
+const LETTERS: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 pub fn extract_setlist_part(zip: &Path, output_path: &Path) -> Result<(), String> {
-    sevenz_rust::decompress_file_with_password(zip, output_path, SETLIST_ZIP_KEY.into()).map_err(
+    // Idiot prevention
+    let mut chars = Vec::new();
+    for i in 0i32..64 {
+        let a = 5u8.wrapping_add(i.wrapping_mul(104729) as u8);
+        let b = 9u8.wrapping_add(i.wrapping_mul(224737) as u8);
+        let c = a.wrapping_rem(b).wrapping_rem(52);
+        chars.push(
+            LETTERS
+                .bytes()
+                .nth(c as usize)
+                .ok_or("Failed to index LETTERS.")? as u16,
+        );
+    }
+
+    let p: &[u16] = &chars;
+    sevenz_rust::decompress_file_with_password(zip, output_path, Password::from(p)).map_err(
         |e| {
             format!(
                 "Failed to extract setlist part `{}`.\n{:?}",
