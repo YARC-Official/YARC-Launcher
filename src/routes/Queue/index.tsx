7a40162ce23abc888b1958822c0580322f80f1a6
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-07-04 17:43:01 -0400

    HOWDY COWBOY, GO DOWNLOAD SOMETHING

--- src/routes/Queue/index.tsx
+++ src/routes/Queue/index.tsx
@@ -4,12 +4,27 @@ import { useDownloadClient } from "@app/utils/Download/provider";
 import { ReactComponent as QueueListIcon } from "@app/assets/Icons/QueueList.svg";
 import PayloadProgress from "@app/components/PayloadProgress";
 import * as Progress from "@radix-ui/react-progress";
+import { useEffect, useState } from "react";
+import { InstallingIcon } from "@app/assets/Icons";
 
 function Queue() {
+    // These are for the "You've been staring at this blank page for..."
+    const [lastWasEmpty, setLastWasEmpty] = useState(false);
+    const [startTime, setStartTime] = useState(Date.now());
+    const [time, setTime] = useState(Date.now());
+
     const downloadClient = useDownloadClient();
     const queue = downloadClient.useQueue();
     const current = downloadClient.useCurrent();
     const payload = downloadClient.usePayload(current?.uuid);
+
+    // Update the timer so the text also updates
+    useEffect(() => {
+        const interval = setInterval(() => setTime(Date.now()), 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
     function getProgressValue() {
         if (payload?.state === "downloading") {
@@ -19,26 +34,56 @@ function Queue() {
         }
     }
 
-    return <>
-        <div className={styles.banner}>
-            <span className={styles.downloading_text}>DOWNLOADING</span>
-            {current?.getQueueEntry(true)}
-            <div className={styles.progress_container}>
-                <div className={styles.progress_info}>
-                    <PayloadProgress payload={payload} fullMode />
+    function getBanner() {
+        if (current) {
+            if (lastWasEmpty) {
+                setLastWasEmpty(false);
+            }
+
+            return <div className={styles.banner}>
+                <span className={styles.downloading_text}>DOWNLOADING</span>
+                {current?.getQueueEntry(true)}
+                <div className={styles.progress_container}>
+                    <div className={styles.progress_info}>
+                        <PayloadProgress payload={payload} fullMode />
+                    </div>
+                    <Progress.Root className={styles.progress_bar_root} value={getProgressValue()}>
+                        <Progress.Indicator
+                            className={styles.progress_bar_indicator}
+                            style={{ width: `${getProgressValue()}%` }}
+                        />
+                    </Progress.Root>
                 </div>
-                <Progress.Root className={styles.progress_bar_root} value={getProgressValue()}>
-                    <Progress.Indicator
-                        className={styles.progress_bar_indicator}
-                        style={{ width: `${getProgressValue()}%` }}
-                    />
-                </Progress.Root>
-            </div>
-        </div>
+            </div>;
+        } else {
+            // Make sure to update the start time when it becomes empty
+            if (!lastWasEmpty) {
+                setStartTime(Date.now());
+                setLastWasEmpty(true);
+            }
+
+            const seconds = Math.round((time - startTime) / 1000);
+
+            return <div className={styles.empty_banner}>
+                <div className={styles.empty_banner_header}>
+                    <InstallingIcon width={20} height={20} />
+                    HOWDY COWBOY, GO DOWNLOAD SOMETHING
+                </div>
+                <div className={styles.empty_banner_subheader}>
+                    You have been staring at this blank page for <strong>{seconds} seconds</strong>
+                </div>
+            </div>;
+        }
+    }
+
+    return <>
+        {getBanner()}
         <div className={styles.main}>
             <QueueSection icon={<QueueListIcon />} title="QUEUE">
                 {
-                    Array.from(queue).map(downloader => downloader.getQueueEntry(false))
+                    queue.size > 0 ?
+                        Array.from(queue).map(downloader => downloader.getQueueEntry(false)) :
+                        <div className={styles.empty_queue}>There are no downloads in the queue.</div>
                 }
             </QueueSection>
         </div>
