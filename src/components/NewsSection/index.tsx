65fa725874ef8fc9c8720278879db4a032740d09
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-07-19 22:52:10 -0400

    Only show three news articles at a time

--- src/components/NewsSection/index.tsx
+++ src/components/NewsSection/index.tsx
@@ -2,9 +2,11 @@ import { NewsIcon } from "@app/assets/Icons";
 import styles from "./NewsSection.module.css";
 import NewsEntry from "./NewsEntry";
 import { useNews } from "@app/hooks/useNews";
+import { useState } from "react";
 
 const NewsSection: React.FC = () => {
     const { data, error, isLoading, isSuccess } = useNews();
+    const [displayCount, setDisplayCount] = useState(4);
 
     if (isLoading) return "Loading..";
 
@@ -18,8 +20,9 @@ const NewsSection: React.FC = () => {
                 </div>
             </div>
             {
-                Array.from(data.articles).map(article => <NewsEntry article={article} key={article.md} />)
+                Array.from(data.articles.slice(0, displayCount)).map(article => <NewsEntry article={article} key={article.md} />)
             }
+            <div className={styles.load_more} onClick={() => setDisplayCount(displayCount + 4)}>Load More...</div>
         </div>;
     }
 };
