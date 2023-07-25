6edea4e2d68390ba002222431d88bf13a0d67c96
Panto! <guustavopantoja@gmail.com>
2023-07-25 17:44:23 -0300

    eslint moment

--- src/components/LaunchButton/index.tsx
+++ src/components/LaunchButton/index.tsx
@@ -35,7 +35,7 @@ export function LaunchButton(props: LaunchButtonProps) {
     if(version.state === YARGStates.PLAYING) {
         return <Button color={ButtonColor.GRAY} style={props.style}>
             Opening YARG {playName}
-        </Button>
+        </Button>;
     }
 
     return <Button style={props.style}>
