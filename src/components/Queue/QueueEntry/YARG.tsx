30ee974a6173a977f5f27f358f50ed4f0f25300f
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2023-08-11 12:56:47 -0400

    Fixed compiler error

--- src/components/Queue/QueueEntry/YARG.tsx
+++ src/components/Queue/QueueEntry/YARG.tsx
@@ -2,6 +2,7 @@ import { YARGDownload } from "@app/utils/Download/Processors/YARG";
 import BaseQueue from "./base";
 import StableYARGIcon from "@app/assets/StableYARGIcon.png";
 import NightlyYARGIcon from "@app/assets/NightlyYARGIcon.png";
+import DevYARGIcon from "@app/assets/DevYARGIcon.png";
 import { YARGChannels } from "@app/hooks/useYARGRelease";
 
 interface Props {
@@ -11,14 +12,15 @@ interface Props {
 
 const YARGQueue: React.FC<Props> = ({ downloader, bannerMode }: Props) => {
 
-    const channelIconPath: {[key in YARGChannels]: string} = {
+    const channelIconPath: { [key in YARGChannels]: string } = {
         "stable": StableYARGIcon,
-        "nightly": NightlyYARGIcon
+        "nightly": NightlyYARGIcon,
+        "newEngine": DevYARGIcon
     };
 
     return <BaseQueue
         name="YARG"
-        icon={<img src={ channelIconPath[downloader.channel] } />}
+        icon={<img src={channelIconPath[downloader.channel]} />}
         version={downloader.version}
         versionChannel={downloader.channel.toUpperCase()}
         bannerMode={bannerMode}
