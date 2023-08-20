e44207230244f8f2375668ad792b4d825ee1456a
Panto! <guustavopantoja@gmail.com>
2023-08-20 02:35:05 -0300

    feat: SortChanger more closer to what's proposed on concept

--- src/routes/Setlist/SortChanger.tsx
+++ src/routes/Setlist/SortChanger.tsx
@@ -1,6 +1,7 @@
 import { SetlistSong } from "@app/hooks/useSetlistRelease";
 import styles from "./SortChanger.module.css";
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
+import { NoteIcon, SongIcon, TimeIcon } from "@app/assets/Icons";
 
 export type SortType = keyof SetlistSong;
 
@@ -16,12 +17,15 @@ const SortChanger: React.FC<Props> = ({ onChange }: Props) => {
         onValueChange={(value) => onChange(value as SortType)}>
         
         <ToggleGroup.Item className={styles.item} value="title">
+            <NoteIcon />
             Song Name
         </ToggleGroup.Item>
         <ToggleGroup.Item className={styles.item} value="artist">
+            <SongIcon />
             Artist Name
         </ToggleGroup.Item>
         <ToggleGroup.Item className={styles.item} value="length">
+            <TimeIcon />
             Song Length
         </ToggleGroup.Item>
     </ToggleGroup.Root>;
