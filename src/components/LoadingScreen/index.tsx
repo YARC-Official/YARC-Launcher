b8d1033589aedd7003211b6550f9e3ac95909eb5
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-01-10 16:33:17 -0500

    Added styling to loading screen

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -1,8 +1,16 @@
 import styles from "./LoadingScreen.module.css";
+import * as Progress from "@radix-ui/react-progress";
 
 const LoadingScreen: React.FC = () => {
     return <div className={styles.container}>
-        <p>Loading...</p>
+        <Progress.Root className={styles.progressRoot}>
+            <Progress.Indicator className={styles.progressIndicator} />
+        </Progress.Root>
+
+        <div className={styles.factContainer}>
+            <p className={styles.factHeader}>Fun Fact</p>
+            YARG stands for Yet Another Rhythm Game
+        </div>
     </div>;
 };
 
