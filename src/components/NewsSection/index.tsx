fbbb2d451882ddf700018a07fab3d05cf4a367da
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-08-17 21:30:28 -0400

    Don't show news in offline mode

--- src/components/NewsSection/index.tsx
+++ src/components/NewsSection/index.tsx
@@ -3,24 +3,36 @@ import styles from "./NewsSection.module.css";
 import NewsEntry from "./NewsEntry";
 import { useNews } from "@app/hooks/useNews";
 import { useState } from "react";
+import { useOfflineStatus } from "@app/hooks/useOfflineStatus";
 
 interface Props {
-    categoryFilter?: string
+    categoryFilter?: string,
     startingEntries?: number
 }
 
 const NewsSection: React.FC<Props> = ({ categoryFilter, startingEntries }: Props) => {
+    const offlineStatus = useOfflineStatus();
     const { data, error, isLoading, isSuccess } = useNews();
     const [displayCount, setDisplayCount] = useState(startingEntries ? startingEntries : 4);
 
-    if (isLoading) return "Loading..";
+    if (offlineStatus.isOffline) {
+        return <div className={styles.offlineContainer}>
+            Offline
+        </div>;
+    }
 
-    if (error) return `An error has occurred: ${error}`;
+    if (isLoading) {
+        return "Loading...";
+    }
+
+    if (error) {
+        return `An error has occurred: ${error}`;
+    }
 
     if (isSuccess) {
         return <div className={styles.container}>
-            <div className={styles.header_container}>
-                <div className={styles.header_text}>
+            <div className={styles.headerContainer}>
+                <div className={styles.headerText}>
                     <NewsIcon width={15} height={15} /> NEWS
                 </div>
             </div>
@@ -31,7 +43,7 @@ const NewsSection: React.FC<Props> = ({ categoryFilter, startingEntries }: Props
                     return i.category === categoryFilter;
                 }).slice(0, displayCount).map(article => <NewsEntry article={article} key={article.md} />)
             }
-            <div className={styles.load_more} onClick={() => setDisplayCount(displayCount + 4)}>Load More...</div>
+            <div className={styles.loadMore} onClick={() => setDisplayCount(displayCount + 4)}>Load More...</div>
         </div>;
     }
 };
