93b74e50421a98417391980fa3d13a52811eff73
EliteAsian <29520859+EliteAsian123@users.noreply.github.com>
2024-09-29 00:52:03 -0400

    Show unknown icon when image cannot be loaded

--- src/components/ProfileIcon/index.tsx
+++ src/components/ProfileIcon/index.tsx
@@ -1,5 +1,7 @@
+import { Img } from "react-image";
 import styles from "./ProfileIcon.module.css";
 import { processAssetUrl } from "@app/profiles/utils";
+import UnknownImage from "@app/assets/Unknown.png";
 
 interface Props {
     iconUrl: string;
@@ -8,7 +10,7 @@ interface Props {
 
 const ProfileIcon: React.FC<Props> = ({ iconUrl, className }: Props) => {
     return <div className={[styles.icon, className].join(" ")}>
-        <img src={processAssetUrl(iconUrl)} />
+        <Img src={[processAssetUrl(iconUrl), UnknownImage]} />
     </div>;
 };
 
