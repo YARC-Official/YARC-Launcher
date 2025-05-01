fe50a340dd72036e0e3912f3267357bbb5a2f542
Anton Romanov <theli.ua@gmail.com>
2025-04-30 20:36:15 -0700

    Fix getOs

--- src/utils/os.ts
+++ src/utils/os.ts
@@ -2,4 +2,11 @@ import { platform } from "@tauri-apps/plugin-os";
 
 export type OS = "windows" | "macos" | "linux";
 
-export const getOS = platform;
+export const getOS = async (): Promise<OS> => {
+    switch (platform()) {
+        case "windows": return "windows";
+        case "macos":     return "macos";
+        case "linux":      return "linux";
+    }
+    throw("Unsupported OS");
+};
