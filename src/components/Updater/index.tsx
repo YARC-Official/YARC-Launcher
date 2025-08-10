5cfafd240e64cb0e1542ba6f9f9d4d29c211149b
wyrdough <nathan@nwacg.net>
2025-08-10 17:27:17 -0400

    Add confirmation dialog before updating
    - User selects OK update is launched; user selects cancel launcher runs as normal.

--- src/components/Updater/index.tsx
+++ src/components/Updater/index.tsx
@@ -1,12 +1,16 @@
 import {check} from "@tauri-apps/plugin-updater";
 import {relaunch} from "@tauri-apps/plugin-process";
+import {confirm} from "@tauri-apps/plugin-dialog";
 
 async function CheckForUpdates() {
     const update = await check();
 
     if (update) {
-        await update.downloadAndInstall();
-        await relaunch();
+        const confirmed = await confirm("Would you like to update now?", "Launcher Update Available");
+        if (confirmed) {
+            await update.downloadAndInstall();
+            await relaunch();
+        }
     }
 }
 
