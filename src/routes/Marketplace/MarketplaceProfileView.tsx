f656f708a727665c96f80d12f0c596e319e5bbc2
The Fat Bastid <tfb@sof.ws>
2026-02-13 17:56:01 -0500

    Added checkmark to marketplace items in library (#98)
    * Added checkmark to marketplace items in library
    
    The checkmark is font dependent and might look different on different systems.
    
    * Changed library venues to show screenshots
    
    Instead of show just venue news
    
    * Removed filter from library application's newsection
    
    * Changed checkmark from font to svg
    
    As it was already in the assets, I just didn't notice.

--- src/routes/Marketplace/MarketplaceProfileView.tsx
+++ src/routes/Marketplace/MarketplaceProfileView.tsx
@@ -3,6 +3,8 @@ import styles from "./Marketplace.module.css";
 import ProfileIcon from "@app/components/ProfileIcon";
 import { localizeObject } from "@app/utils/localized";
 import { processAssetUrl } from "@app/profiles/utils";
+import { useProfileStore } from "@app/profiles/store";
+import {CheckmarkIcon} from "@app/assets/Icons";
 
 interface Props {
     profile: MarketplaceProfile,
@@ -10,7 +12,10 @@ interface Props {
 }
 
 const MarketplaceProfileView: React.FC<Props> = ({ profile, setSelectedProfile }: Props) => {
+    const profiles = useProfileStore();
+    const isInstalled = profiles.anyOfProfileUUID(profile.uuid);
     const localized = localizeObject(profile, "en-US");
+
     let bannerUrl = localized.bannerUrl;
     if (bannerUrl === undefined) {
         bannerUrl = localized.iconUrl;
@@ -24,10 +29,14 @@ const MarketplaceProfileView: React.FC<Props> = ({ profile, setSelectedProfile }
         <ProfileIcon iconUrl={localized.iconUrl} className={styles.icon} />
         <div className={styles.info}>
             <header>{localized.name}</header>
-            {localized.subText !== undefined &&
-                <span>{localized.subText}</span>
-            }
+            {localized.subText !== undefined && <span>{localized.subText}</span>}
         </div>
+
+        {isInstalled && (
+            <span className={styles.installedLabel}>
+                <CheckmarkIcon className={styles.checkmarkIcon} />
+            </span>
+        )}
     </button>;
 };
 
